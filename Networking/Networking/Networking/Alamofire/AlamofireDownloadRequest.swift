49270558c667b21faa441b41c24c560a00c04c57
Amaan Ullah <amaankhanse@gmail.com>
2019-04-09 14:14:19 +0500

    download request comments added.

--- Networking/Networking/Networking/Alamofire/AlamofireDownloadRequest.swift
+++ Networking/Networking/Networking/Alamofire/AlamofireDownloadRequest.swift
@@ -10,8 +10,16 @@ import Foundation
 import Alamofire
 
 
+// MARK: DownloadRequest
+
 extension Alamofire.DownloadRequest: DownloadableRequest {
     
+    
+    
+    /// Handler to be called once the request has finished.
+    ///
+    /// - Parameters:
+    ///   - completion: The code to be executed once the request has finished.
     
     func response(completion: @escaping DownloadResponseClosure) {
         let _ :Alamofire.DownloadRequest = response { (response) in
@@ -19,7 +27,13 @@ extension Alamofire.DownloadRequest: DownloadableRequest {
             completion(downloadResponse)
         }
     }
+
     
+    
+    /// Periodically calls the closure as data is read from the server.
+    ///
+    /// - Parameters:
+    ///   - progress: The code to be executed periodically once the request has finished.
     
     func progress(_ progress: @escaping DownloadProgressClosure) {
         downloadProgress { (downloadProgress) in
@@ -28,22 +42,41 @@ extension Alamofire.DownloadRequest: DownloadableRequest {
     }
     
     
+    
     func resume(data: Data, completion: @escaping DownloadResponseClosure) {
             
     }
+    
+    
+    /// Validates that the response has a status code in the specified sequence.
+    ///
+    /// - Parameters:
+    ///   - validationType: The `ValidationType`.
     
     func validate(validationType: ValidationType) {
         validate(statusCode: validationType.statusCodes)
     }
     
     
+    
+    /// Cancels the Request.
+
     func cancel() {
         let _: Alamofire.DownloadRequest = cancel()
     }
     
 }
 
+
+
+
+// MARK: DownloadResponse
+
 extension Alamofire.DownloadResponse: DownloadResponse {
+    
+    
+    /// The destination URL of the data returned from the server.
+
     var destinationURL: URL? {
         return fileURL
     }
